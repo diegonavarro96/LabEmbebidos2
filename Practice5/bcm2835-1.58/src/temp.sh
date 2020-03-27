363e4f83c97a48dd553ba9d78658b21c95f4ae02
mvilla06 <mauricio.villalpando_06@hotmail.com>
2020-03-27 16:42:46 -0600

    Build records script

--- Practice5/bcm2835-1.58/src/temp.sh
+++ Practice5/bcm2835-1.58/src/temp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-records=(1 0 0)
-r=("01/01/01 Mon 12:00:00" "01/01/01 Mon 12:00:00" "01/01/01 Mon 12:00:00")
+index=0
+days=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
+r=("01/01/01 Monday 12:00:00" "01/01/01 Monday 12:00:00" "01/01/01 Monday 12:00:00")
 
 ./i2c -s77 -dw -ib 1 0x00
 temp=`./i2c -s77 -dr -ib 1`
+temp=$(( 16#$temp ))
 echo "RECEIVER> Temperature $temp C\n" >>data.txt
 
 if [$temp -gt 35]
@@ -16,21 +18,10 @@ then
     date=`./i2c -s104 -dr -ib 1`
     month=`./i2c -s104 -dr -ib 1`
     year=`./i2c -s104 -dr -ib 1`
-
-    for i in $records    
-    do
-        if [${records[$i]} -eq 1]
-        then
-            r[$i]="$year/$month/$date $day $hr:$min:$sec"
-            index=$i
-        fi
-    done
-
-    ${records[$index]} = 0
-    if [$index -eq 2]
-    then
-        records[0]=1
-    else
-        records[${index+1}]=1
-    fi
+   
+    r[$index]="$year/$month/$date ${days[$day]} $hr:$min:$sec"
+    
+    index=${index+1}
+    index=$((index%3))
 fi
+
