40264fc06e748564b344e062fb0cd62ffc99f872
mvilla06 <mauricio.villalpando_06@hotmail.com>
2020-03-27 17:58:08 -0600

    Mensaje

--- Practice5/bcm2835-1.58/src/temp.sh
+++ Practice5/bcm2835-1.58/src/temp.sh
@@ -5,7 +5,7 @@ days=("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
 r=("01/01/01 Monday 12:00:00" "01/01/01 Monday 12:00:00" "01/01/01 Monday 12:00:00")
 
 while sleep 5; do
-./i2c -s77 -dw -ib 1 0x00
+./i2c -s77 -dw -ib 1 0x00>>basura.txt
 temp=`./i2c -s77 -dr -ib 1`
 echo '1' $temp
 
@@ -17,27 +17,27 @@ echo "RECEIVER> Temperature $temp C\n" >> data.txt
 
 if [ $temp -gt 35 ]
 then
-    ./i2c -s104 -dw -ib 1 0x00
+    ./i2c -s104 -dw -ib 1 0x00>>basura.txt
     sec=`./i2c -s104 -dr -ib 1`
     sec=${sec:43:2}
 #    echo 'sec =' $sec
-    ./i2c -s104 -dw -ib 1 0x01
+    ./i2c -s104 -dw -ib 1 0x01>>basura.txt
     min=`./i2c -s104 -dr -ib 1`
     min=${min:43:2}
  #   echo 'min =' $min
-    ./i2c -s104 -dw -ib 1 0x02
+    ./i2c -s104 -dw -ib 1 0x02>>basura.txt
     hr=`./i2c -s104 -dr -ib 1`
     hr=${hr:43:2}
-    ./i2c -s104 -dw -ib 1 0x03
+    ./i2c -s104 -dw -ib 1 0x03>>basura.txt
     day=`./i2c -s104 -dr -ib 1`
     day=${day:43:2}
-    ./i2c -s104 -dw -ib 1 0x04
+    ./i2c -s104 -dw -ib 1 0x04>>basura.txt
     date=`./i2c -s104 -dr -ib 1`
     date=${date:43:2}
-    ./i2c -s104 -dw -ib 1 0x05
+    ./i2c -s104 -dw -ib 1 0x05>>basura.txt
     month=`./i2c -s104 -dr -ib 1`
     month=${month:43:2}
-    ./i2c -s104 -dw -ib 1 0x06
+    ./i2c -s104 -dw -ib 1 0x06>>basura.txt
     year=`./i2c -s104 -dr -ib 1`
     year=${year:43:2}
   #  echo 'year = ' $year    
@@ -47,6 +47,7 @@ then
     echo ${r[2]} >> data.txt
     index=${index+1}
     index=$((index%3))
-fi
 
+fi
+rm basura.txt
 done
